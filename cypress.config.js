56a70fec4557b0bf76f7fc37e4f7dc2a54922933
LisaPaliashchuk <yelizaveta.paliashchuk@Orchestr8.com>
2024-04-26 13:55:20 +0200

    updated package.json and cypress.config.js to include dashboard configs (HW_5.2.1)

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'qy8d5s',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
